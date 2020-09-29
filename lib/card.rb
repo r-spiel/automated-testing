438fece0bdb1420ac423a55e6a4a01bd40830718
r-spiel <richelle.mehlhaff@gmail.com>
2020-09-28 20:32:35 -0700

    update card class methods and card_tests for reader and testing to_s method

--- lib/card.rb
+++ lib/card.rb
@@ -1,17 +1,32 @@
 
 # card.rb
+VALID_SUITES = [:hearts, :spades, :clubs, :diamonds]
 
 class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
+    raise ArgumentError.new("Invalid values") if value <= 0 || value > 13
+    raise ArgumentError.new("Invalid suit") if !VALID_SUITES.include?(suit)
+
     @value = value
     @suit = suit
-    
 
   end
 
+  def convert_to_face_card(value)
+
+    face_cards = { 1 => "Ace", 11 => "Jack", 12 => "Queen", 13 => "King" }
+    if face_cards.keys.include?(value)
+      face_card = face_cards[value]
+    end
+
+    return face_card
+  end
+
   def to_s
+    convert_to_face_card(value)
+
     return "#{value} of #{suit.to_s}"
   end
 
--- test/card_test.rb
+++ test/card_test.rb
@@ -24,32 +24,37 @@ describe Card do
   describe "Testing to_s" do
 
     it "to_s returns a readable String value logically for values 2-10" do
-      # Test to ensure that to_s works for cards values 2-10
-      # for example:  "2 of diamonds"
+      [:hearts, :spades, :clubs, :diamonds].each do |suit|
+        (1..13).each do |value|
+          card = Card.new(value, suit)
+          expect(card.to_s).must_equal "#{card.value} of #{card.suit}"
+        end
+      end
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
-      # Test to ensure that to_s works for cards values 1, and 11-13
-      # For example: "Queen of hearts"
-      # The current implementation of to_s does not address this feature
-      # Write the test, see it fail, then modify to_s to make it pass!
-      # (Consider writing a helper method!)
-      #
-      # For reference:
-      #  1: Ace
-      #  11: Jack
-      #  12: Queen
-      #  13: King
+      [:hearts, :spades, :clubs, :diamonds].each do |suit|
+        [1, 11, 12, 13].each do |value|
+          card = Card.new(value, suit)
+          expect(card.to_s).must_equal "#{card.value} of #{card.suit}"
+        end
+      end
+
     end
   end
 
   describe "Reader methods" do
 
+    let(:eight_spades){
+      Card.new(8, :spades)
+    }
     it "Can retrieve the value of the card using a `.value`." do
+      expect(eight_spades.value).must_equal 8
       # ensure that `.value works as expected`
     end
 
     it "Can retrieve the value of the card using a `.suit`." do
+      expect(eight_spades.suit).must_equal :spades
       # ensure that `.suit works as expected returning the symbol of the suit`
 
     end
